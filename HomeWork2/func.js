6c344d035ebde7c15c8d504854f67f6aff33a70e
alexryapolov <alexryapolov36@gmail.com>
2021-06-23 22:50:01 +0400

    complete home work 2

---
+++ HomeWork2/func.js
@@ -0,0 +1,19 @@
+// Функция которая принимает 2 числа и возвращает их сумму
+function getSum(num1, num2) {
+  return num1 + num2;
+}
+
+// Функция которая принимает 2 числа и возвращает их разность
+function getSub(num1, num2) {
+  return num1 - num2;
+}
+
+// Функция которая принимает 2 числа и возвращает их произведение
+function getMil(num1, num2) {
+  return num1 * num2;
+}
+
+// Функция которая принимает 2 числа и возвращает их сумму частное
+function getDiv(num1, num2) {
+  return num1 / num2;
+}
